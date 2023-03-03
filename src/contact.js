30aeac7037ceb8c7505e62fe4ecff909f9799eb0
Juno <doplitog@gmail.com>
2023-03-03 15:27:47 -0800

    add contact page and loading function

---
+++ src/contact.js
@@ -0,0 +1,83 @@
+function createContact() {
+
+  const contact = document.createElement('div')
+  contact.classList.add('contact')
+
+  const header = document.createElement('div');
+  header.classList.add('header');
+
+  const heading = document.createElement('h1');
+  heading.textContent = 'Contact Us';
+
+  header.appendChild(heading);
+
+  
+  const contactContainer = document.createElement("div");
+  const phone = document.createElement("p");
+  phone.textContent =
+    "Phone: (123) 456-7890";
+
+  const email = document.createElement("p");
+  email.textContent =
+    "Email: customerservice@sapporosushi.com";
+
+  const address = document.createElement("p");
+  address.textContent =
+    "Address: 1234 Main St, Suite 69, Somewhere USA 12345";
+
+  contactContainer.appendChild(phone);
+  contactContainer.appendChild(email);
+  contactContainer.appendChild(address);
+
+  contact.appendChild(header);
+  contact.appendChild(contactContainer);
+
+  
+  return contact;
+}
+
+/* // function to create a div with the class header and attach the h1 text to the div for contact page
+function Header() {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const heading = document.createElement('h1');
+    heading.textContent = 'Contact Us';
+
+    header.appendChild(heading);
+
+    return header;
+} */
+
+// function to create a div containing contact information for the restaurant
+/* function Contact() {
+  const contactContainer = document.createElement("div");
+  const phone = document.createElement("p");
+  phone.textContent =
+    "Phone: (123) 456-7890";
+
+  const email = document.createElement("p");
+  email.textContent =
+    "Email: customerservice@sapporosushi.com";
+
+  const address = document.createElement("p");
+  address.textContent =
+    "Address: 1234 Main St, Suite 69, Somewhere USA 12345";
+
+  contactContainer.appendChild(phone);
+  contactContainer.appendChild(email);
+  contactContainer.appendChild(address);
+
+  return contactContainer;
+} */
+
+// function to createContact selects div id='content' and then calls the Header and Contact functions
+// to grab the generated elements and append them to the content div for contact page
+
+function loadContact() {
+  const content = document.querySelector('#content');
+  content.innerHTML = '';
+  content.appendChild(createContact());
+}
+
+export default loadContact;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import _ from 'lodash';
 import sushiImg from './sushi.png';
+import loadContact from './contact.js';
 
 
 // function to create a navbar module
 function createNav() {
   const nav = document.createElement('nav');
-  nav.innerHTML = `
-    <ul>
-      <li><a id='home-link' href='#'>Home</a></li>
-      <li><a id='menu-link' href='#'>Menu</a></li>
-      <li><a id='contact-link' href='#'>Contact</a></li>
-    <ul>
-  `;
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("button-nav");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+
+  nav.appendChild(contactButton);
 
   return nav;
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
 }
 
 // function to create a div with the class header and attach the h1 text to the div for home page
