6676e62a463fecac03dbb35762f504c3e406e465
Juno <doplitog@gmail.com>
2023-03-03 12:30:34 -0800

    finish home page and add navbar

---
+++ src/index.js
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+import sushiImg from './sushi.png';
+
+
+// function to create a navbar module
+function createNav() {
+  const nav = document.createElement('nav');
+  nav.innerHTML = `
+    <ul>
+      <li><a id='home-link' href='#'>Home</a></li>
+      <li><a id='menu-link' href='#'>Menu</a></li>
+      <li><a id='contact-link' href='#'>Contact</a></li>
+    <ul>
+  `;
+
+  return nav;
+}
+
+// function to create a div with the class header and attach the h1 text to the div for home page
+function Header() {
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const heading = document.createElement('h1');
+    heading.textContent = 'Sapporo Sushi';
+
+    header.appendChild(heading);
+
+    return header;
+}
+
+// function to create a div containing an image for home page
+function Image() {
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = sushiImg;
+    img.alt = 'sushi';
+    imgContainer.appendChild(img);
+
+    return imgContainer;
+}
+
+// function to create a div containg a <p> with copy for home page
+function Copy() {
+  const copyContainer = document.createElement("div");
+  const copy = document.createElement("p");
+  copy.textContent =
+    "Sapporo Sushi is a contemporary restaurant that specializes in serving authentic Japanese cuisine. Located in the heart of the city, the restaurant offers a sophisticated and stylish dining experience that's perfect for any occasion. From sushi rolls to sashimi, the menu at Sapporo Sushi features a variety of fresh and delicious dishes that are prepared with the finest ingredients. Whether you're looking for a romantic dinner for two or a group gathering with friends, Sapporo Sushi is the perfect destination to satisfy your cravings for delectable Japanese food. Come and experience the flavors of Japan at Sapporo Sushi!";
+
+  copyContainer.appendChild(copy);
+
+  return copyContainer;
+}
+
+// content selects div id='content' and then calls the 3 above functions
+// to have the generated and appends them to the content div for home page
+const content = document.querySelector('#content');
+const navElement = createNav();
+const headerElement = Header();
+const imgElement = Image();
+const copyElement = Copy();
+
+// append the navbar, header, image, and copy to the content div
+content.appendChild(navElement);
+content.appendChild(headerElement);
+content.appendChild(imgElement);
+content.appendChild(copyElement);
+
