80bcf68df3195fb448641627850bb0d04c1cac5e
Juno <doplitog@gmail.com>
2023-03-04 01:47:32 -0800

    Finish styling and clean up code
    Finsihed the styling, need to do a little more clean up, and
    figure out how to get the github icon to work.

--- src/index.js
+++ src/index.js
@@ -10,29 +10,29 @@ window.addEventListener('load', loadHome);
 function createNav() {
   const nav = document.createElement('nav');
 
-  const homeButton = document.createElement("button");
-  homeButton.classList.add("button-nav");
-  homeButton.textContent = "Home";
-  homeButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  const homeButton = document.createElement('button');
+  homeButton.classList.add('button-nav');
+  homeButton.textContent = 'Home';
+  homeButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(homeButton);
     loadHome();
   });
 
-  const menuButton = document.createElement("button");
-  menuButton.classList.add("button-nav");
-  menuButton.textContent = "Menu";
-  menuButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  const menuButton = document.createElement('button');
+  menuButton.classList.add('button-nav');
+  menuButton.textContent = 'Menu';
+  menuButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(menuButton);
     loadMenu();
   });
 
-  const contactButton = document.createElement("button");
-  contactButton.classList.add("button-nav");
-  contactButton.textContent = "Contact";
-  contactButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  const contactButton = document.createElement('button');
+  contactButton.classList.add('button-nav');
+  contactButton.textContent = 'Contact';
+  contactButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(contactButton);
     loadContact();
   });
@@ -46,15 +46,15 @@ function createNav() {
 }
 
 function setActiveButton(button) {
-  const buttons = document.querySelectorAll(".button-nav");
+  const buttons = document.querySelectorAll('.button-nav');
 
   buttons.forEach((button) => {
     if (button !== this) {
-      button.classList.remove("active");
+      button.classList.remove('active');
     }
   });
 
-  button.classList.add("active");
+  button.classList.add('active');
 }
 
 // function to create a div with the class header and attach h1 and navbar to it.
@@ -86,8 +86,8 @@ function createHome() {
   img.alt = 'sushi';
   imgContainer.appendChild(img);
   
-  const copyContainer = document.createElement("div");
-  const copy = document.createElement("p");
+  const copyContainer = document.createElement('div');
+  const copy = document.createElement('p');
   copy.textContent =
     "Sapporo Sushi offers a sophisticated and stylish dining experience that's perfect for any occasion. From sushi rolls to sashimi, the menu at Sapporo Sushi features a variety of fresh and delicious dishes that are prepared with the finest ingredients. Whether you're looking for a romantic dinner for two or a group gathering with friends, Sapporo Sushi is the perfect destination to satisfy your cravings for delectable Japanese food. Come and experience the flavors of Japan at Sapporo Sushi!";
 
@@ -105,4 +105,26 @@ function loadHome() {
   content.appendChild(createHome());
 }
 
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
+
+  const copyright = document.createElement('p');
+  copyright.textContent = `Copyright © ${new Date().getFullYear()} Nbk_Juno`;
+
+  const githubLink = document.createElement('a');
+  githubLink.href = 'https://github.com/Juno-png';
+
+  const githubIcon = document.createElement('i');
+  githubIcon.classList.add('fa-brands', 'fa-github');
+
+
+  githubLink.appendChild(githubIcon);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+
+  return footer;
+}
+
 document.body.insertBefore(Header(), content);
+document.body.insertAdjacentElement('beforeend', createFooter());
